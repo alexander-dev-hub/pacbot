0b819091728eb370893dc795bcad018d85bdf24e
Alexander <wangming19871126@gmail.com>
2019-09-02 19:29:43 +0700

    first commit

---
+++ installer/resources/lambda_submit/function.py
@@ -0,0 +1,197 @@
+from core.terraform.resources.aws.aws_lambda import LambdaFunctionResource, LambdaPermission
+from core.terraform.resources.aws.cloudwatch import CloudWatchEventRuleResource, CloudWatchEventTargetResource
+from resources.datastore.es import ESDomainPolicy
+from resources.datastore.es import ESDomain
+from resources.datastore.db import MySQLDatabase
+from resources.iam.lambda_role import LambdaRole
+from resources.iam.base_role import BaseRole
+from resources.s3.bucket import BucketStorage
+from resources.batch.job import SubmitAndRuleEngineJobDefinition, BatchJobsQueue
+from resources.data.aws_info import AwsAccount, AwsRegion
+from resources.lambda_submit.s3_upload import UploadLambdaSubmitJobZipFile, BATCH_JOB_FILE_NAME
+from resources.pacbot_app.alb import ApplicationLoadBalancer
+import json
+
+
+class SubmitJobLambdaFunction(LambdaFunctionResource):
+    function_name = "datacollector"
+    role = LambdaRole.get_output_attr('arn')
+    handler = BATCH_JOB_FILE_NAME + ".lambda_handler"
+    runtime = "python2.7"
+    s3_bucket = BucketStorage.get_output_attr('bucket')
+    s3_key = UploadLambdaSubmitJobZipFile.get_output_attr('id')
+    environment = {
+        'variables': {
+            'JOB_QUEUE': BatchJobsQueue.get_input_attr('name'),
+            'JOB_DEFINITION': SubmitAndRuleEngineJobDefinition.get_output_attr('arn'),
+            'CONFIG_URL': ApplicationLoadBalancer.get_api_base_url() + "/config/batch,inventory/prd/latest",
+            'CONFIG_CREDENTIALS': "dXNlcjpwYWNtYW4=",
+            'CONFIG_SERVICE_URL': ApplicationLoadBalancer.get_http_url() + "/api/config/rule/prd/latest"
+        }
+    }
+
+    DEPENDS_ON = [SubmitAndRuleEngineJobDefinition, BatchJobsQueue]
+
+
+class DataCollectorEventRule(CloudWatchEventRuleResource):
+    name = "AWS-Data-Collector"
+    schedule_expression = "cron(0 * * * ? *)"
+
+    DEPENDS_ON = [SubmitJobLambdaFunction]
+
+
+class DataCollectorEventRuleLambdaPermission(LambdaPermission):
+    statement_id = "AllowExecutionFromDataCollectorEvent"
+    action = "lambda:InvokeFunction"
+    function_name = SubmitJobLambdaFunction.get_output_attr('function_name')
+    principal = "events.amazonaws.com"
+    source_arn = DataCollectorEventRule.get_output_attr('arn')
+
+
+class DataCollectorCloudWatchEventTarget(CloudWatchEventTargetResource):
+    rule = DataCollectorEventRule.get_output_attr('name')
+    arn = SubmitJobLambdaFunction.get_output_attr('arn')
+    target_id = 'DataCollectorTarget'  # Unique identifier
+    target_input = json.dumps({
+        'jobName': "AWS-Data-Collector",
+        'jobUuid': "pacman-aws-inventory-jar-with-dependencies",
+        'jobType': "jar",
+        'jobDesc': "AWS-Data-Collection",
+        'environmentVariables': [
+            {'name': "CONFIG_URL", 'value': ApplicationLoadBalancer.get_api_base_url() + "/config/batch,inventory/prd/latest"},
+            {'name': "CONFIG_CREDENTIALS", 'value': "dXNlcjpwYWNtYW4="},
+            {'name': "CONFIG_SERVICE_URL", 'value': ApplicationLoadBalancer.get_http_url() + "/api/config/rule/prd/latest"}
+        ],
+        'params': [
+            {'encrypt': False, 'key': "package_hint", 'value': "com.tmobile.cso.pacman"},
+            {'encrypt': False, 'key': "config_creds", 'value': "dXNlcjpwYWNtYW4="},
+            {'encrypt': False, 'key': "accountinfo", 'value': AwsAccount.get_output_attr('account_id')},
+        ]
+    })
+
+
+class DataShipperEventRule(CloudWatchEventRuleResource):
+    name = "aws-redshift-es-data-shipper"
+    schedule_expression = "cron(0 * * * ? *)"
+
+    DEPENDS_ON = [SubmitJobLambdaFunction, ESDomainPolicy]
+
+
+class DataShipperEventRuleLambdaPermission(LambdaPermission):
+    statement_id = "AllowExecutionFromDataShipper"
+    action = "lambda:InvokeFunction"
+    function_name = SubmitJobLambdaFunction.get_output_attr('function_name')
+    principal = "events.amazonaws.com"
+    source_arn = DataShipperEventRule.get_output_attr('arn')
+
+
+class DataShipperCloudWatchEventTarget(CloudWatchEventTargetResource):
+    rule = DataShipperEventRule.get_output_attr('name')
+    arn = SubmitJobLambdaFunction.get_output_attr('arn')
+    target_id = 'DataShipperTarger'  # Unique identifier
+    target_input = json.dumps({
+        'jobName': "aws-redshift-es-data-shipper",
+        'jobUuid': "data-shipper-jar-with-dependencies",
+        'jobType': "jar",
+        'jobDesc': "Ship aws data periodically from redshfit to ES",
+        'environmentVariables': [
+            {'name': "CONFIG_URL", 'value': ApplicationLoadBalancer.get_api_base_url() + "/config/batch,data-shipper/prd/latest"},
+            {'name': "ASSET_API_URL", 'value': ApplicationLoadBalancer.get_api_version_url('asset')},
+            {'name': "CMPL_API_URL", 'value': ApplicationLoadBalancer.get_api_version_url('compliance')},
+            {'name': "AUTH_API_URL", 'value': ApplicationLoadBalancer.get_api_version_url('auth')},
+            {'name': "CONFIG_CREDENTIALS", 'value': "dXNlcjpwYWNtYW4="},
+            {'name': "CONFIG_SERVICE_URL", 'value': ApplicationLoadBalancer.get_http_url() + "/api/config/rule/prd/latest"}
+
+        ],
+        'params': [
+            {'encrypt': False, 'key': "package_hint", 'value': "com.tmobile"},
+            {'encrypt': False, 'key': "datasource", 'value': "aws"},
+            {'encrypt': False, 'key': "config_creds", 'value': "dXNlcjpwYWNtYW4="},
+            {'encrypt': False, 'key': "apiauthinfo",
+                'value': "MjJlMTQ5MjItODdkNy00ZWU0LWE0NzAtZGEwYmIxMGQ0NWQzOmNzcldwYzVwN0pGRjR2RVpCa3dHQ0FoNjdrR1FHd1h2NDZxdWc3djVad3RLZw=="}
+        ]
+    })
+
+
+class RecommendationsCollectorEventRule(CloudWatchEventRuleResource):
+    name = "AWS-Recommendations-Collector"
+    schedule_expression = "cron(0 * * * ? *)"
+
+    DEPENDS_ON = [SubmitJobLambdaFunction]
+
+
+class RecommendationsCollectorEventRuleLambdaPermission(LambdaPermission):
+    statement_id = "AllowExecutionFromRecommendationsCollectorEvent"
+    action = "lambda:InvokeFunction"
+    function_name = SubmitJobLambdaFunction.get_output_attr('function_name')
+    principal = "events.amazonaws.com"
+    source_arn = RecommendationsCollectorEventRule.get_output_attr('arn')
+
+
+class RecommendationsCollectorCloudWatchEventTarget(CloudWatchEventTargetResource):
+    rule = RecommendationsCollectorEventRule.get_output_attr('name')
+    arn = SubmitJobLambdaFunction.get_output_attr('arn')
+    target_id = 'RecommendationsCollectorTarget'  # Unique identifier
+    target_input = json.dumps({
+        'jobName': "aws-recommendations-collector",
+        'jobUuid': "recommendation-enricher-jar-with-dependencies",
+        'jobType': "jar",
+        'jobDesc': "Index trusted advisor checks as recommendations",
+        'environmentVariables': [
+            {'name': "CONFIG_URL", 'value': ApplicationLoadBalancer.get_api_base_url() + "/config/batch,recommendation-enricher/prd/latest"},
+            {'name': "PACMAN_API_URI", 'value': ApplicationLoadBalancer.get_api_base_url()},
+            {'name': "LOGGING_ES_HOST_NAME", 'value': ESDomain.get_http_url_with_port()},
+            {'name': "ES_URI", 'value': ESDomain.get_http_url_with_port()},
+            {'name': "ENVIRONMENT", 'value': "prd"},
+            {'name': "APP_NAME", 'value': "aws-recommendations-collector"},
+            {'name': "APP_TYPE", 'value': "etl"},
+            {'name': "HEIMDALL_URI", 'value': ESDomain.get_http_url_with_port()},
+            {'name': "BASE_AWS_ACCOUNT", 'value': AwsAccount.get_output_attr('account_id')},
+        ],
+        'params': [
+            {'encrypt': False, 'key': "package_hint", 'value': "com.tmobile.cso.pacbot"},
+            {'encrypt': False, 'key': "config_creds", 'value': "dXNlcjpwYWNtYW4="},
+        ]
+    })
+
+
+class CloudNotificationCollectorEventRule(CloudWatchEventRuleResource):
+    name = "AWS-CloudNotification-Collector"
+    schedule_expression = "cron(0 * * * ? *)"
+
+    DEPENDS_ON = [SubmitJobLambdaFunction]
+
+
+class CloudNotificationCollectorEventRuleLambdaPermission(LambdaPermission):
+    statement_id = "AllowExecutionFromCloudNotificationCollectorEvent"
+    action = "lambda:InvokeFunction"
+    function_name = SubmitJobLambdaFunction.get_output_attr('function_name')
+    principal = "events.amazonaws.com"
+    source_arn = CloudNotificationCollectorEventRule.get_output_attr('arn')
+
+
+class CloudNotificationCollectorCloudWatchEventTarget(CloudWatchEventTargetResource):
+    rule = CloudNotificationCollectorEventRule.get_output_attr('name')
+    arn = SubmitJobLambdaFunction.get_output_attr('arn')
+    target_id = 'CloudNotificationCollectorTarget'  # Unique identifier
+    target_input = json.dumps({
+        'jobName': "aws-cloud-notification-collector",
+        'jobUuid': "pacman-cloud-notifications-jar-with-dependencies",
+        'jobType': "jar",
+        'jobDesc': "Health Notification Collector",
+        'environmentVariables': [
+            {'name': "CONFIG_URL", 'value': ApplicationLoadBalancer.get_api_base_url() + "/config/api/prd/latest"},
+            {'name': "PACMAN_API_URI", 'value': ApplicationLoadBalancer.get_api_base_url()},
+            {'name': "LOGGING_ES_HOST_NAME", 'value': ESDomain.get_http_url_with_port()},
+            {'name': "ES_URI", 'value': ESDomain.get_http_url_with_port()},
+            {'name': "ENVIRONMENT", 'value': "prd"},
+            {'name': "APP_NAME", 'value': "aws-cloud-notification-collector"},
+            {'name': "APP_TYPE", 'value': "etl"},
+            {'name': "BASE_AWS_ACCOUNT", 'value': AwsAccount.get_output_attr('account_id')},
+        ],
+        'params': [
+            {'encrypt': False, 'key': "package_hint", 'value': "com.tmobile"},
+            {'encrypt': False, 'key': "config_creds", 'value': "dXNlcjpwYWNtYW4="},
+            {'encrypt': False, 'key': "conf_src", 'value': "api-prd,application-prd"},
+        ]
+    })
---
+++ installer/settings/default.local.py
@@ -0,0 +1,46 @@
+# CRAETE local.py file by renaming/copying default.local.py
+# User should update the VPC details below in local.py
+VPC = {
+    "ID": "vpc-1",
+    "CIDR_BLOCKS": ["10.0.0.0/16"],
+    "SUBNETS": ["subnet-1", "subnet-2"]
+}
+
+
+# CUstom tags that can be defined by user
+CUSTOM_RESOURCE_TAGS = [
+    {'Application': "PacBot"},
+    {'Environment': "Prod"}
+]
+
+
+# RDS Related Configurations
+RDS_INSTANCE_TYPE = "db.t2.medium"  # Possibble values db.m4.large, db.t2.large etc
+
+
+# ElasticSearch Related Configurations
+ES_INSTANCE_TYPE = "m4.large.elasticsearch"  # Possibble values m4.xlarge.elasticsearch, t2.xlarge.elasticsearch etc
+
+
+# ALB related configurations
+MAKE_ALB_INTERNAL = True  # False if ALB need to be public(internet facing) else True
+ALB_PROTOCOL = "HTTP"
+SSL_CERTIFICATE_ARN = ""  # Required only if ALB_PROTOCOL is defined as HTTPS
+PACBOT_DOMAIN = ""  # Required only if you point a CNAME record to ALB ex: app.pacbot.com
+
+# MAIL Server configuration
+MAIL_SERVER = "localhost"
+MAIL_SERVER_PORT = 587
+MAIL_PROTOCOL = "smtp"
+MAIL_SERVER_USER = ""
+MAIL_SERVER_PWD = ""
+MAIL_SMTP_AUTH = ""
+MAIL_SMTP_SSL_ENABLE = "true"
+MAIL_SMTP_SSL_TEST_CONNECTION = "false"
+
+USER_EMAIL_ID = ""
+
+# System reads below data from user if not updated here
+AWS_ACCESS_KEY = ""
+AWS_SECRET_KEY = ""
+AWS_REGION = ""
